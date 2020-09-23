1dbace53fcd1ddb61f6f3c1db5c5bb9a1e4d6269
Niktos112 <nik.tos1510@gmail.com>
2020-09-23 10:00:05 +0300

    Added new translator

--- Перевод от 11 до 35.py
+++ Перевод от 11 до 35.py
@@ -1,5 +1,8 @@
 def decimal_translator(number, base):
+    letters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     for i in str(number):
+        if i.isalpha():
+            i = letters.find(i)
         if int(i) >= base:
             return None
     nums = []
